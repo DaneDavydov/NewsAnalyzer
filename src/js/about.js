5862a6ef7156d4d60980a915d7dc3d9abd53d75f
Denis Davydov <dane.tno@gmail.com>
2020-02-26 15:32:14 +0400

    js git

--- src/js/about.js
+++ src/js/about.js
@@ -2,21 +2,14 @@ import '../pages/about.css';
 import 'flickity/dist/flickity.min.css';
 import '../blocks/flickity-custom/flickity-custom.css';
 import Flickity from 'flickity';
-import GithudApi from './modules/GithubApi';
-import CommitsCard from './components/CommitCard';
+// import GithudApi from './modules/GithubApi';
+import CommitCard from './components/CommitCard';
 import CommitsCardList from './components/CommitCardList';
 
-//api
-const api = new Api({
-  url: '',
-  headers: {
-      authorization: '',
-      'Content-Type': 'application/json'
-  }
-});
-
 //карточка
-const card = new CommitsCard();
+const card = new CommitCard();
 
 //место под карточки
-const cardList = new CommitsCardList(document.querySelector(".flickity-slider"), card, api);
+const cardList = new CommitsCardList(document.querySelector(".slider__carousel"), card);
+
+cardList.render();
--- src/js/components/CommitCardList.js
+++ src/js/components/CommitCardList.js
@@ -1,6 +1,32 @@
+// const GITHUB_DATA = {
+//   name: 'commit.committer.name',
+//   email: 'commit.committer.email',
+//   date: 'commit.committer.date',
+//   message: 'commit.message',
+//   avatar: 'author.avatar_url'
+// };
+
 export default class CommitCardList {
-  constructor() {
-
+  constructor(container, cardTemplate) {
+    this.container = container;
+    this.cardTemplate = cardTemplate;
   }
-
+  addCard(name, email, date, message, avatar) {
+    const card = this.cardTemplate.createCommitCard(name, email, date, message, avatar);
+    this.container.insertAdjacentHTML('afterbegin', card)
+  }
+  // render() {
+  //   this.cardTemplate.forEach(card => {
+  //     this.addCard(
+  //       'commit.committer.name',
+  //       'commit.committer.email',
+  //       'commit.committer.date',
+  //       'commit.message',
+  //       'author.avatar_url'
+  //     );
+  //   });
+  // }
 }
+
+//  Можно лучше: В качестве параметров передавайте не переменные, а объект
+//  если вы в ходе развития проекта захотите добавить переменных, то вам придётся менять код во многих местах
